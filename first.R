ab797b8230644245251d59765ae3cfaabb5abee4
J.M. Morales <pajarom@gmail.com>
2020-05-12 10:19:12 +0100

    smaller

--- first.R
+++ first.R
@@ -1,10 +1,3 @@
 library(rstan) # observe startup messages
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
-
-C = as.matrix(read.csv('https://github.com/jmmorales/lipids/raw/master/data/C.csv'))
-Ys = read.csv('https://github.com/jmmorales/lipids/raw/master/data/Ys.csv')
-TT = as.matrix(read.csv('https://github.com/jmmorales/lipids/raw/master/data/TT.csv'))
-X = as.matrix(read.csv('https://github.com/jmmorales/lipids/raw/master/data/X.csv'))
-Dmat = as.matrix(read.csv('https://github.com/jmmorales/lipids/raw/master/data/Dmat.csv'))
-DistP = as.matrix(read.csv('https://github.com/jmmorales/lipids/raw/master/data/DistP.csv'))
